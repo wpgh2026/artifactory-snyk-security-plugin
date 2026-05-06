011738baeb5a2c5c3685e921d878e962a5d685a2
wpgh2026 <wpgithub@arly.au>
2026-05-06 11:47:57 +0800

    Update SnykClient.java

--- snyk-sdk/src/main/java/io/snyk/sdk/api/SnykClient.java
+++ snyk-sdk/src/main/java/io/snyk/sdk/api/SnykClient.java
@@ -8,10 +8,10 @@ import org.slf4j.LoggerFactory;
 
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
-import javax.net.Authenticator;
-import javax.net.PasswordAuthentication;
 import javax.net.ssl.X509TrustManager;
 import java.io.IOException;
+import java.net.Authenticator;
+import java.net.PasswordAuthentication;
 import java.net.InetSocketAddress;
 import java.net.ProxySelector;
 import java.net.URLEncoder;
